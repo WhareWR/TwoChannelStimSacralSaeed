b2c114d7dc06333edb311beb38b7f3c9213baf5f
WhareWR <wharengaror@gmail.com>
2022-05-17 12:47:44 +1200

    Comment

--- TwoChannelStimSacral.cs
+++ TwoChannelStimSacral.cs
@@ -257,6 +257,7 @@ namespace SignalGenerator
                 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            //New Comment
             RealTimeChange = !RealTimeChange;
         }
         
